994fad28e545403f9e1cfb66a58c2a082724d59c
paul xavier perrine <pxperrine@gmail.com>
2026-02-18 00:19:20 +0100

    update blankenv.py

--- backend/blankenv.py
+++ backend/blankenv.py
@@ -17,3 +17,7 @@ DB_USER = "postgres"  # ← your database user
 DB_PASSWORD = "password"  # ← your database password
 DB_HOST = "192.168.0.120"  # ← your database host
 DB_PORT = "5433"  # ← your database port
+
+
+REDIS_URL = "redis://192.168.0.225:6379"  # for connection manager, watcher.py
+REDIS_URL2 = "192.168.0.225"  # for arq
