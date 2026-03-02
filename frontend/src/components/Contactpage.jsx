98f82b6cfd7334292e49b3b54251d974b9496ac5
paul xavier perrine <pxperrine@gmail.com>
2026-03-02 09:36:30 +0100

    fix contactpage bug

--- frontend/src/components/Contactpage.jsx
+++ frontend/src/components/Contactpage.jsx
@@ -28,13 +28,13 @@ function Contactpage() {
     try {
       console.log("Submitting contact form to ntfy:", formData);
 
-      const response = await fetch("http://51.20.201.88:8000/partyShotsContact", {
+      const response = await fetch("http://172.17.0.1:8000/partyShotsContact", {
         method: "POST",
         body: `From: ${formData.email}\nSubject: ${formData.subject}\nSource: ${sourceUrl}\n\n${formData.body}`,
         headers: {
-          "Title": `Contact Form: ${formData.subject}`,
-          "Tags": "email,mailbox_with_mail",
-        }
+          Title: `Contact Form: ${formData.subject}`,
+          Tags: "email,mailbox_with_mail",
+        },
       });
 
       if (!response.ok) {
@@ -122,4 +122,3 @@ function Contactpage() {
 }
 
 export default Contactpage;
-
