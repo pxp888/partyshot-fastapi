b44db778712f1cc41df96437a319dc6b72f6bd1e
paul xavier perrine <pxperrine@gmail.com>
2026-03-03 10:08:03 +0100

    welcome css

--- frontend/src/components/CookieConsent.jsx
+++ frontend/src/components/CookieConsent.jsx
@@ -3,22 +3,22 @@ import { useMessage } from "./MessageBoxContext";
 import { getCookie, setCookie } from "./helpers";
 
 const CookieConsent = () => {
-    const { showMessage } = useMessage();
+  const { showMessage } = useMessage();
 
-    useEffect(() => {
-        const consent = getCookie("cookie_consent");
-        if (!consent) {
-            showMessage(
-                "This site uses session cookies for the distribution network to work. We do not use cookies for tracking or marketing.",
-                "Cookie Policy",
-                () => {
-                    setCookie("cookie_consent", "true", 365);
-                }
-            );
-        }
-    }, [showMessage]);
+  useEffect(() => {
+    const consent = getCookie("cookie_consent");
+    if (!consent) {
+      showMessage(
+        "This site uses session cookies to store login information. We do not use cookies for tracking or marketing.",
+        "Cookie Policy",
+        () => {
+          setCookie("cookie_consent", "true", 365);
+        },
+      );
+    }
+  }, [showMessage]);
 
-    return null; // This component doesn't render anything itself
+  return null; // This component doesn't render anything itself
 };
 
 export default CookieConsent;
